8b18e03e93668f8fffdbdd85c9ea2cf85da650e0
Victor Hernandez <v.hernandez@cgiar.org>
2022-10-26 17:03:15 -0500

    nav vertical responsive

--- src/InventarioGEI/InventarioGEI/wwwroot/js/site.js
+++ src/InventarioGEI/InventarioGEI/wwwroot/js/site.js
@@ -2,3 +2,41 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const showNavbar = (toggleId, navId, bodyId, headerId) => {
+        const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId),
+            bodypd = document.getElementById(bodyId),
+            headerpd = document.getElementById(headerId)
+
+        // Validate that all variables exist
+        if (toggle && nav) {
+            toggle.addEventListener('click', () => {
+                // show navbar
+                nav.classList.toggle('show')
+                // change icon
+                //toggle.classList.toggle('bx-x')
+                // add padding to body
+                bodypd.classList.toggle('content-pd')
+            })
+        }
+    }
+
+    showNavbar('sidebarCollapse', 'nav-bar', 'content', 'header')
+
+    /*===== LINK ACTIVE =====*/
+    const linkColor = document.querySelectorAll('.nav_link')
+
+    function colorLink() {
+        if (linkColor) {
+            linkColor.forEach(l => l.classList.remove('active'))
+            this.classList.add('active')
+        }
+    }
+    linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+    // Your code to run since DOM is loaded and ready
+});
