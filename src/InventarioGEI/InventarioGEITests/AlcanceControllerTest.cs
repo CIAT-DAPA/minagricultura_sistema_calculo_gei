969051c9a204f126a4750e87a94c8ab1c8e3e40f
Sebastian Lopez <sebastian.lopez_ast@uao.edu.co>
2023-01-17 16:10:53 -0500

    pruebas de roles y alcances

---
+++ src/InventarioGEI/InventarioGEITests/AlcanceControllerTest.cs
@@ -0,0 +1,160 @@
+﻿using InventarioGEI.Controllers;
+using InventarioGEI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioGEITests
+{
+    public class AlcanceControllerTest
+    {
+        public AlcancesController controller;
+        public Context _dbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            var rol = new Rol { idRol = 1, nombreRol = "Rol1test", permisoConfiguracion = true, permisoRol = true, enabled = true };
+            var userTest = new Usuario { idUsuario = 1, email = "adminTest@admin.com", idRol = 1, enabled = true };
+            var alcanceToEdit = new Alcance { idAlcance = 5, nombreAlcance = "alcanceTest", isBiocombustible = false, enabled = true, usuario = userTest};
+
+
+            var optionsBuilder = new DbContextOptionsBuilder<Context>();
+            optionsBuilder.UseInMemoryDatabase("DBTest");
+            _dbContext = new Context(optionsBuilder.Options);
+            _dbContext.Rol.Add(rol);
+            _dbContext.Alcance.Add(alcanceToEdit);
+            _dbContext.Usuario.Add(userTest);
+            _dbContext.SaveChangesAsync();
+            controller = new AlcancesController(_dbContext);
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                            {
+                                new Claim(ClaimTypes.Name, "adminTest@admin.com"),
+
+                            }, "mock"));
+            controller.ControllerContext.HttpContext = new DefaultHttpContext() { User = user };
+        }
+
+        [Test]
+        public async Task TestIndexView()
+        {
+            var result = await controller.Index();
+            var viewResult = result as ViewResult;
+
+            Assert.IsNotNull(viewResult);
+            Assert.Greater(((IEnumerable<Alcance>)viewResult.Model).Count(), 0);
+        }
+
+        [Test]
+        public async Task TestCreateView()
+        {
+            var result = controller.Create();
+            var viewResult = result as ViewResult;
+
+            Assert.IsNotNull(viewResult);
+        }
+
+        [Test]
+        public async Task TestCreateCorrect()
+        {
+            var alcance = new Alcance { idAlcance = 2, nombreAlcance = "Rol2test", isBiocombustible = true, enabled = true };
+
+            var result = await controller.Create(alcance);
+            var redirectResult = result as RedirectToActionResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", redirectResult.ActionName);
+        }
+
+        [Test]
+        public async Task TestCreateIncorrect()
+        {
+            var alcance = new Alcance { isBiocombustible = true, enabled = true };
+            controller.ModelState.AddModelError("nombreAlcance", "Es necesario un nombre para el alcance");
+
+            var result = await controller.Create(alcance);
+            var redirectResult = result as RedirectToActionResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsNull(redirectResult);
+        }
+
+        [Test]
+        public async Task TestEditView()
+        {
+            var result = await controller.Edit(5);
+            var viewResult = result as ViewResult;
+
+            Assert.IsNotNull(viewResult);
+        }
+
+        [Test]
+        public async Task TestEditAnIDDoesntExist()
+        {
+            var result = await controller.Edit(76);
+
+            Assert.AreEqual(404, ((NotFoundResult)result).StatusCode);
+        }
+
+        [Test]
+        public async Task TestEditCorrect()
+        {
+            _dbContext.Entry(_dbContext.Alcance.Find(5)).State = EntityState.Detached;
+            var alcanceEdited = new Alcance { idAlcance = 5, nombreAlcance = "RolEditado", isBiocombustible = true, enabled = true };
+            var result = await controller.Edit(5, alcanceEdited);
+
+            var redirectResult = result as RedirectToActionResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", redirectResult.ActionName);
+        }
+
+        [Test]
+        public async Task TestEditIncorrect()
+        {
+            var alcance = new Alcance { idAlcance = 5, isBiocombustible = true, enabled = true };
+            controller.ModelState.AddModelError("nombreAlcance", "Es necesario un nombre para el alcance");
+
+            var result = await controller.Edit(5, alcance);
+            var redirectResult = result as RedirectToActionResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsNull(redirectResult);
+        }
+
+        [Test]
+        public async Task TestDeleteView()
+        {
+            var result = await controller.Delete(5);
+            var viewResult = result as ViewResult;
+
+            Assert.IsNotNull(viewResult);
+        }
+
+        [Test]
+        public async Task TestDeleteAnIDDoesntExist()
+        {
+            var result = await controller.Delete(76);
+
+            Assert.AreEqual(404, ((NotFoundResult)result).StatusCode);
+        }
+
+        [Test]
+        public async Task TestDelete()
+        {
+            _dbContext.Entry(_dbContext.Alcance.Find(5)).State = EntityState.Detached;
+            var result = await controller.DeleteConfirmed(5);
+
+            var redirectResult = result as RedirectToActionResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", redirectResult.ActionName);
+        }
+    }
+}
--- src/InventarioGEI/InventarioGEITests/RolControllerTest.cs
+++ src/InventarioGEI/InventarioGEITests/RolControllerTest.cs
@@ -13,18 +13,20 @@ namespace InventarioGEITests
     public class RolControllerTest
     {
         public RolsController controller;
-
+        public Context _dbContext;
         [SetUp]
         public void Setup()
         {
             var rol = new Rol {idRol = 1, nombreRol = "Rol1test", permisoConfiguracion = true, permisoRol = true, enabled=true};
+            var rolToEdit = new Rol {idRol = 5, nombreRol = "RolEdit", permisoConfiguracion = true, permisoRol = true, permisoRegistro=false ,enabled=true};
 
             var userTest = new Usuario {idUsuario = 1, email = "adminTest@admin.com", idRol=1, enabled=true};
 
             var optionsBuilder = new DbContextOptionsBuilder<Context>();
             optionsBuilder.UseInMemoryDatabase("DBTest");
-            var _dbContext = new Context(optionsBuilder.Options);
+            _dbContext = new Context(optionsBuilder.Options);
             _dbContext.Rol.Add(rol);
+            _dbContext.Rol.Add(rolToEdit);
             _dbContext.Usuario.Add(userTest);
             _dbContext.SaveChangesAsync();
             controller = new RolsController(_dbContext);
@@ -56,7 +58,7 @@ namespace InventarioGEITests
         }
 
         [Test]
-        public async Task TestCreateRolCorrect()
+        public async Task TestCreateCorrect()
         {
             var rol = new Rol { idRol = 2, nombreRol = "Rol2test", permisoConfiguracion = true, permisoRol = true, enabled = true };
 
@@ -68,15 +70,89 @@ namespace InventarioGEITests
         }
 
         [Test]
-        public async Task TestCreateRolIncorrect()
+        public async Task TestCreateIncorrect()
         {
-            var rol = new Rol { permisoConfiguracion = true, permisoRol = true, enabled = true };
+            var rol = new Rol {permisoConfiguracion = true, permisoRol = true, enabled = true };
+            controller.ModelState.AddModelError("nombreRol", "Es necesario un nombre para el rol");
 
             var result = await controller.Create(rol);
             var redirectResult = result as RedirectToActionResult;
 
             Assert.IsNotNull(result);
+            Assert.IsNull(redirectResult);
+        }
+
+        [Test]
+        public async Task TestEditView()
+        {
+            var result = await controller.Edit(5);
+            var viewResult = result as ViewResult;
+
+            Assert.IsNotNull(viewResult);
+        }
+
+        [Test]
+        public async Task TestEditAnIDDoesntExist()
+        {
+            var result = await controller.Edit(76);
+            
+            Assert.AreEqual(404, ((NotFoundResult)result).StatusCode);
+        }
+
+        [Test]
+        public async Task TestEditCorrect()
+        {
+            _dbContext.Entry(_dbContext.Rol.Find(5)).State = EntityState.Detached;
+            var rolEdited = new Rol { idRol = 5, nombreRol = "RolEditado", permisoConfiguracion = true, permisoRol = true, permisoRegistro = false, enabled = true };
+            var result = await controller.Edit(5, rolEdited);
+
+            var redirectResult = result as RedirectToActionResult;
+
+            Assert.IsNotNull(result);
             Assert.AreEqual("Index", redirectResult.ActionName);
         }
+
+        [Test]
+        public async Task TestEditIncorrect()
+        {
+            var rol = new Rol { idRol = 5, permisoConfiguracion = true, permisoRol = true, permisoRegistro = false, enabled = true };
+            controller.ModelState.AddModelError("nombreRol", "Es necesario un nombre para el rol");
+
+            var result = await controller.Edit(5, rol);
+            var redirectResult = result as RedirectToActionResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsNull(redirectResult);
+        }
+
+        [Test]
+        public async Task TestDeleteView()
+        {
+            var result = await controller.Delete(5);
+            var viewResult = result as ViewResult;
+
+            Assert.IsNotNull(viewResult);
+        }
+
+        [Test]
+        public async Task TestDeleteAnIDDoesntExist()
+        {
+            var result = await controller.Delete(76);
+
+            Assert.AreEqual(404, ((NotFoundResult)result).StatusCode);
+        }
+
+        [Test]
+        public async Task TestDelete()
+        {
+            _dbContext.Entry(_dbContext.Rol.Find(5)).State = EntityState.Detached;
+            var result = await controller.DeleteConfirmed(5);
+
+            var redirectResult = result as RedirectToActionResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", redirectResult.ActionName);
+        }
+
     }
 }
